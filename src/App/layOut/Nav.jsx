df9ff4b3766153c922ca5a98e8bb15babe64b676
bazz <clement.f.bazz@gmail.com>
2022-11-12 12:38:08 +0100

    Imported Nav link and Button Component

--- src/App/layOut/Nav.jsx
+++ src/App/layOut/Nav.jsx
@@ -1,12 +1,19 @@
+import { NavLink } from "react-router-dom";
 import Icons from "../../assets/icons/Icons";
+import Button from "../component/Button";
 
 export default function Nav() {
         return (
                 <nav className="nav">
-                        <div className="container--global">
-                                <Icons name='meta-mask' />
-                                <Icons name='brand' />
-                                <Icons name='brand-white' />
+                        <div className="container--global nav-container">
+                                <Icons name='brand' className='nav__logo' />
+                                <ul className="nav-link__list" role={'list'}>
+                                        <li><NavLink to={'/'}>Home </NavLink>  </li>
+                                        <li><NavLink to={'/'}>Place to Stay </NavLink>  </li>
+                                        <li><NavLink to={'/'}>NFTs </NavLink>  </li>
+                                        <li><NavLink to={'/'}>Community </NavLink>  </li>
+                                </ul>
+                                <Button />
                         </div>
                 </nav>
         )
