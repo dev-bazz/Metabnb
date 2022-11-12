502926401228157a3ecb78be6ddf1dab74d2c72a
bazz <clement.f.bazz@gmail.com>
2022-11-12 08:52:34 +0100

    Modified:  Application Icons
    Add SVG into One COmponent

--- src/App/layOut/Nav.jsx
+++ src/App/layOut/Nav.jsx
@@ -3,7 +3,9 @@ import Icons from "../../assets/icons/Icons";
 export default function Nav() {
         return (
                 <div>
-                        <Icons name='heart' />
+                        <Icons name='star' />
+                        <Icons name='brand' />
+                        <Icons name='brand-white' />
                 </div>
         )
 }
--- src/assets/icons/Icons.jsx
+++ src/assets/icons/Icons.jsx
@@ -1,15 +1,52 @@
-import { BrandLogo, Star, Heart } from "./BrandSet"
+import { BrandLogo, Star, Heart, Wallet, BrandWhite, X, Setting, Facebook, Twitter, Instagran, OpenSea, MetaMask, MBToken } from "./BrandSet";
+
+
 const Icons = ({ name, ...rest }) => {
         switch (name) {
                 case "brand":
                         return <BrandLogo {...rest} />;
+
                 case "star":
                         return <Star />;
+
                 case "heart":
                         return <Heart />;
+
+                case "brand-white":
+                        return <BrandWhite />;
+
+                case "arrow-up":
+                        return <Arrow />;
+
+                case "cross":
+                        return <X />;
+
+                case "settings":
+                        return <Setting />;
+
+                case "facebook":
+                        return <Facebook />
+
+                case "twitter":
+                        return <Twitter />
+
+                case "Instagran":
+                        return <Instagran />
+
+                case "wallet":
+                        return <Wallet />
+
+                case "open-sea":
+                        return <OpenSea />
+
+                case "meta-mask":
+                        return <MetaMask />
+
+                case "mb-token":
+                        return <MBToken />
+
                 default:
                         return <span>No Icon Selected</span>;
         }
 }
-
 export default Icons
