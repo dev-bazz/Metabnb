2c2d9bc07c8db64ba27b5b6f64f5f1e97add63de
bazz <clement.f.bazz@gmail.com>
2022-11-13 22:10:01 +0100

    UI Update

--- src/App/pages/Nft.jsx
+++ src/App/pages/Nft.jsx
@@ -1,17 +1,38 @@
+import Icons from "../../assets/icons/Icons";
+import {allNFT} from "../../assets/images/imageNFTs";
+import NftCard from "../component/NtfCard";
 
 export default function Nft() {
         return (
                 <main className="container--global nft-page">
-                        <ul role={`list`} className={`nav-list`}>
-                                <l1>Resturant</l1>
-                                <l1>Cottage</l1>
-                                <l1>Castle</l1>
-                                <l1>fantast city</l1>
-                                <l1>beach</l1>
-                                <l1>Carbins</l1>
-                                <l1>Off-grid</l1>
-                                <l1>Farm</l1>
-                        </ul>
+                        <section className="top">
+                                <ul role={`list`} className={`nav-list`}>
+                                        <l1>Resturant</l1>
+                                        <l1>Cottage</l1>
+                                        <l1>Castle</l1>
+                                        <l1>fantast city</l1>
+                                        <l1>beach</l1>
+                                        <l1>Carbins</l1>
+                                        <l1>Off-grid</l1>
+                                        <l1>Farm</l1>
+                                </ul>
+                                <div className="location">
+                                        <p>Location</p> <Icons name={`settings`} />
+                                </div>
+                        </section>
+                        <section>
+                                <ul className="nft-list" role={`list`}>
+                                        {allNFT.map((nft, index) => <NftCard
+                                                image={nft.image}
+                                                title={nft.title}
+                                                alt={nft.alt}
+                                                availability={nft.availability}
+                                                distance={nft.distance}
+                                                cost={nft.cost}
+                                                key={index}
+                                        />)}
+                                </ul>
+                        </section>
                 </main>
         )
 }
