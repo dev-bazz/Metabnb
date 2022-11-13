692d6001ddcfe2f25d0f61c1ec4e0ed07024513b
bazz <clement.f.bazz@gmail.com>
2022-11-13 22:09:45 +0100

    UI Completed

--- src/App/pages/Nft.jsx
+++ src/App/pages/Nft.jsx
@@ -1,8 +1,17 @@
 
 export default function Nft() {
         return (
-                <main className="container--global">
-                        Nft
+                <main className="container--global nft-page">
+                        <ul role={`list`} className={`nav-list`}>
+                                <l1>Resturant</l1>
+                                <l1>Cottage</l1>
+                                <l1>Castle</l1>
+                                <l1>fantast city</l1>
+                                <l1>beach</l1>
+                                <l1>Carbins</l1>
+                                <l1>Off-grid</l1>
+                                <l1>Farm</l1>
+                        </ul>
                 </main>
         )
 }
