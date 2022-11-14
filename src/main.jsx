94abeeeceff2b1aee3e2f7cc4564e80dcb9fb032
bazz <clement.f.bazz@gmail.com>
2022-11-14 09:40:32 +0100

    Hash Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 // React Dependecies
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom"
+import { HashRouter as Router } from "react-router-dom"
 
 // App component
 import App from "./App/App.jsx";
